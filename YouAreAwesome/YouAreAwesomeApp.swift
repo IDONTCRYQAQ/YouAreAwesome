077bc58b66a678c2114db56bdaad71b4f77832bf
IDONTCRYQAQ <omgisgg@gmail.com>
2023-04-05 19:38:54 +0800

    Initial Commit

---
+++ YouAreAwesome/YouAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeApp.swift
+//  YouAreAwesome
+//
+//  Created by csapp sicp on 2023/4/5.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
