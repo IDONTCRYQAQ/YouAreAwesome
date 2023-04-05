ff1a33809b66f6994cf10a3d26aeddcfd4776771
IDONTCRYQAQ <omgisgg@gmail.com>
2023-04-06 00:49:03 +0800

    Version 1.5

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -8,14 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var messageString = ""
+    @State private var messageString = "Yo!"
     
     var body: some View {
         VStack {
             Text(messageString)
                 .font(.largeTitle)
                 .fontWeight(.heavy)
+                .minimumScaleFactor(0.5)
+                .multilineTextAlignment(.center)
                 .foregroundColor(.red)
+                .frame(height: 150)
+                .frame(maxWidth: .infinity)
+                .border(.orange, width: 1)
                 .padding()
             
             HStack {
@@ -31,7 +36,7 @@ struct ContentView: View {
                 }
                 .buttonStyle(.borderedProminent)
             }
-
+            .border(.purple, width: 5)
         }
     }
 }
